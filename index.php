700f01794e74fada8c3a80b201c2f8d360992a6a
attikapuspitasari <attikapuspitasari@gmail.com>
2020-10-19 12:28:17 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo "Attika P" ?></title>
+    </head>
+    <body>
+        <?php
+            echo "<img src="https://media.giphy.com/media/H4DjXQXamtTiIuCcRU/giphy.gif" alt="Kucing Lucu"/><br />";
+            echo "<h1>Attika Puspitasari</h1>";
+            echo "<h2>1522261205</h2>";
+            echo "<h2>Cloud Computing ITB 2020</h2>";
+        ?>
+    </body>
+</html>
